aa6a0b7d88fd7e09a062970174ff9f2b654ce7a9
Chris <chris.anthony.yee@gmail.com>
2018-04-09 20:18:28 -0700

    added collisions

--- pong.js
+++ pong.js
@@ -34,9 +34,7 @@ var animate = window.requestAnimationFrame ||
    window.onload = function() {
        document.body.appendChild(canvas);
        animate(step)
-       player.paddle.render();
-       computer.paddle.render();
-       ball.render();
+
    };
 
 
@@ -56,37 +54,47 @@ var animate = window.requestAnimationFrame ||
    });
 
    var update = function() {
-     ball.update();
+     ball.update(player.paddle, computer.paddle);
    };
 
    Ball.prototype.update = function(paddle1, paddle2) {
      this.x += this.x_speed;
      this.y += this.y_speed;
-     var top_x = this.x -5;
-     var top_y = this.y -5;
-     var bottom_x = this.x + 5;
-     var bottom_y = this.y + 5;
+     var top_x = this.x - 8;
+     var top_y = this.y - 8;
+     var bottom_x = this.x + 8;
+     var bottom_y = this.y + 8;
 
      //hitting bottom wall
-     if (this.y - 5 < 400) {
-       this.y = 5;
+     if(this.y - 8 > 400) {
+       this.y= 392;
        this.y_speed = -this.y_speed;
-     }else if (this.y + 5 < 0) { //top wall
-       this.y = 395;
-       this.x_speed = -this.x_speed;
+     }else if (this.y + 8 < 0) { //top wall
+       this.y = 8;
+       this.y_speed = -this.y_speed;
      }
 
      if(this.x < 0 || this.x > 800 ){
        //point scored
+       this.x_speed = -3;
        this.y_speed = 0;
-       this.x_speed = 3;
        this.x = 400;
        this.y = 200;
      }
 
-     if () {
+       if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x){
+         //player paddle collision
+         this.x_speed = 3;
+         this.y_speed += (paddle1.y_speed / 2);
+         this.x += this.x_speed;
+       }
+       if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x){
+         //computer paddle
+         this.x_speed = -3;
+         this.y_speed +=(paddle2.y_speed / 2);
+         this.x += this.x_speed;
+       }
 
-     }
    };
 
    var update = function(){
@@ -156,7 +164,7 @@ var animate = window.requestAnimationFrame ||
    function Ball(x, y) {
        this.x = x;
        this.y = y;
-       this.x_speed = 3;
+       this.x_speed = -3;
        this.y_speed = 0;
        this.radius = 8;
    }
